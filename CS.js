e6acbc251cdfa55205c11919937c8a9412d6e9f9
tar <tar@localhost.localdomain>
2017-09-11 12:59:52 +0300

    first commit

---
+++ CS.js
@@ -0,0 +1,11 @@
+alert ("Programmers are so cute!!!");
+
+const salary1 = 1000; // Programmer's 1 salary per month //
+const salary2 = 785;
+const salary3 = 1350;
+if (salary1 + salary2 > salary3);
+console.log ("go to party");
+const cost1 = 455;
+const cost2 = 639;
+console.log ((cost1 + cost2) / 12 );
+console.log (salary3 -(cost1 + cost2 )); // Programmer's 3 remainder per month // 
